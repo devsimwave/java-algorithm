3ef5bd6ac7963e5a500be4aa412b68c91547fcf8
sooyoung <sim980416@gmail.com>
2024-10-08 19:15:16 +0900

    Programmers begin

---
+++ src/PCCE_기출문제_1번.java
@@ -0,0 +1,11 @@
+public class PCCE_기출문제_1번 {
+    public static void main(String[] args) throws Exception {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        
+
+
+    }
+}
