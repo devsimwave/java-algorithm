6f2a44e4be67733d7dc03c3aa34ad9ca2be6d27d
sooyoung <sim980416@gmail.com>
2024-10-24 00:54:53 +0900

    프로그래머스 day3

--- src/programmers/level0/PCCE_기출문제_1번.java
+++ src/programmers/level0/PCCE_기출문제_1번.java
@@ -1,5 +1,4 @@
 package src.programmers.level0;
-
 public class PCCE_기출문제_1번 {
     public static void main(String[] args) throws Exception {
         String msg = "Spring is beginning";
---
+++ src/programmers/level0/몫구하기.java
@@ -0,0 +1,21 @@
+package src.programmers.level0;
+
+public class 몫구하기 {
+
+    public static void main(String[] args) {
+        
+        몫구하기 a = new 몫구하기();
+
+        System.out.println(a.solution(0, 0));
+        
+
+    }
+
+
+    public int solution(int num1, int num2) {
+        
+        int answer = num1 / num2;
+        return answer;
+    }
+    
+}
