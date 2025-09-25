ab25aadf71b18f4384faf548fa14b1e98e37dd01
sooyoung <sim980416@gmail.com>
2025-09-25 23:00:00 +0900

    프로그래머스 day 31 queue/stack remind

--- src/programmers/highscore_kit/stack_queue/level2/기능개발/Solution.java
+++ src/programmers/highscore_kit/stack_queue/level2/기능개발/Solution.java
@@ -27,6 +27,10 @@ public class Solution {
 
         Queue<Integer> queue = new LinkedList<>();
 
+        /*
+         *  남은 기간을 먼저 계산한 뒤 나눠서 0이 되지 않는 기간은 
+         *  그 다음날이 되어야 하니 + 1일을 하기 위해 Math.ceil을 이용해 올림 처리한다.
+         */ 
         for(int i = 0; i < progresses.length; i++) {
             int remain = 100 - progresses[i];
             int days = (int) Math.ceil((double) remain / speeds[i]);
@@ -48,6 +52,37 @@ public class Solution {
         }
 
         // array에 반환
-        return result.stream().mapToInt(x -> x.intValue()).toArray();
+        return queue.stream().mapToInt(Integer::intValue).toArray();
     }
+
+    // 다시 풀어본 문제
+    // public int[] solution(int[] progresses, int[] speeds) {
+    
+    //     Queue<Integer> queue = new LinkedList<>();
+        
+    //     for(int i = 0; i < progresses.length; i++) {
+
+    //         if((100 - progresses[i]) % speeds[i] == 0) {
+    //             queue.offer((100 - progresses[i]) / speeds[i]);
+    //         } else {
+    //             queue.offer((100 - progresses[i]) / speeds[i] + 1);
+    //         }
+
+    //     }
+
+    //     List<Integer> result = new ArrayList<>();
+        
+    //     while (!queue.isEmpty()) {
+    //         int standard = queue.poll();
+    //         int deploy = 1;
+    //         while (!queue.isEmpty() && queue.peek() <= standard) {
+    //             queue.poll();
+    //             deploy++;
+                
+    //         }
+    //         result.add(deploy);
+    //     }
+
+    //     return result.stream().mapToInt(Integer::intValue).toArray();
+    // }
 }
